1f33eab177a25a43429ac76f79a9163453a04356
Kunal2652 <kunal10943@gmail.com>
2025-04-02 03:10:35 +0530

    Finished

---
+++ app/src/main/java/com/example/digividya/class10/ceng10.kt
@@ -0,0 +1,11 @@
+package com.example.digividya.class10
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digividya.R
+
+class ceng10: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.eng10)
+}}
---
+++ app/src/main/java/com/example/digividya/class10/csst10.kt
@@ -0,0 +1,11 @@
+package com.example.digividya.class10
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digividya.R
+
+class csst10 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sst10)
+    }}
